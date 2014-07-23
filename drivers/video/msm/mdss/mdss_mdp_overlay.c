32dff502e644572bf0c61e9511c7cc3a26e2f5a7
Jayant Shekhar <jshekhar@codeaurora.org>
2014-07-23 19:45:20 +0530

    msm: mdss: Ensure pipe are unstaged before destroy
    There are corner cases when pipes are still staged after
    recovery sequence is over in overlay cleanup. Due to this
    when destroying pipe, if pipe fetch halt tries to halt
    the pipe, kernel bug happens and system gets crashed.
    Hence, ensure pipes are unstaged before destroy.
    
    Change-Id: If13ca0fce35656ecbc8f3c98e3017ac981dede06
    Signed-off-by: Jayant Shekhar <jshekhar@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -923,6 +923,8 @@ static void mdss_mdp_overlay_cleanup(struct msm_fb_data_type *mfd,
 			__mdss_mdp_overlay_free_list_add(mfd, &pipe->front_buf);
 		mdss_mdp_data_free(&pipe->back_buf);
 		list_del_init(&pipe->list);
+		if (recovery_mode)
+			mdss_mdp_mixer_pipe_unstage(pipe);
 		mdss_mdp_pipe_destroy(pipe);
 	}
 	mutex_unlock(&mdp5_data->list_lock);
