6ec7217317e4a49058588377f353abc2a7019234
Adrian Salido-Moreno <adrianm@codeaurora.org>
2014-10-17 12:31:47 -0700

    msm: mdss: avoid race conditions during fb release
    During fb release we should release all resources that are associated to
    that process. However, in current release function this can lead to
    kickoff being done, if this is done while display thread is running
    there can be race condition between the updates from two different
    threads. Fix this by pushing the commit through the display thread in
    cases where display thread should be running, or otherwise shutdown this
    thread and do it along with panel blanking.
    
    Change-Id: I9ff6316c18adfdb67d0250a144ddc2f9f2634273
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1549,6 +1549,8 @@ done:
  *
  * Release any resources allocated by calling process, this can be called
  * on fb_release to release any overlays/rotator sessions left open.
+ *
+ * Return number of resources released
  */
 static int __mdss_mdp_overlay_release_all(struct msm_fb_data_type *mfd,
 	bool release_all)
@@ -1588,9 +1590,6 @@ static int __mdss_mdp_overlay_release_all(struct msm_fb_data_type *mfd,
 	}
 	mutex_unlock(&mdp5_data->ov_lock);
 
-	if (cnt)
-		mfd->mdp.kickoff_fnc(mfd, NULL);
-
 	list_for_each_entry_safe(rot, tmp, &mdp5_data->rot_proc_list, list) {
 		if (rot->pid == pid) {
 			if (!list_empty(&rot->list))
@@ -1599,7 +1598,7 @@ static int __mdss_mdp_overlay_release_all(struct msm_fb_data_type *mfd,
 		}
 	}
 
-	return 0;
+	return cnt;
 }
 
 static int mdss_mdp_overlay_play_wait(struct msm_fb_data_type *mfd,
