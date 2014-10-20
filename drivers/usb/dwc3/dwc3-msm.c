0ecca513105599bb011da6f9712c3f91dd703900
myfluxi <linflux@arcor.de>
2014-10-20 21:35:30 +0200

    usb: dwc3: Fix race between pm_resume and runtime_resume

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3436,7 +3436,6 @@ static int dwc3_msm_pm_resume(struct device *dev)
 
 	dev_dbg(dev, "dwc3-msm PM resume\n");
 
-	atomic_set(&mdwc->pm_suspended, 0);
 	if (mdwc->resume_pending) {
 		mdwc->resume_pending = false;
 
@@ -3456,6 +3455,8 @@ static int dwc3_msm_pm_resume(struct device *dev)
 							DWC3_EVENT_XCEIV_STATE);
 		}
 	}
+
+	atomic_set(&mdwc->pm_suspended, 0);
 
 	return ret;
 }
