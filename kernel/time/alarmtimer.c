7777f08dfa233b3e38fefdb90e8e7abdd8f6b744
Lijuan Gao <lijuang@codeaurora.org>
2014-12-24 05:51:00 -0500

    alarmtimer: set power off alarm to be triggered on time
    As the new design of power-off alarm, it's not necessary to boot up
    the phone 2 minutes earlier than the actual power-off alarm time. So
    remove this limit here.
    
    Change-Id: I10c47d191d62452a14253db00cb8922368aa9216
    Signed-off-by: lijuang <lijuang@codeaurora.org>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -28,8 +28,6 @@
 
 #include <mach/cpuidle.h>
 
-#define ALARM_DELTA 120
-
 /**
  * struct alarm_base - Alarm timer bases
  * @lock:		Lock for syncrhonized access to the base
@@ -79,7 +77,7 @@ void power_on_alarm_init(void)
 	rtc_tm_to_time(&rt, &alarm_time);
 
 	if (alarm_time)
-		power_on_alarm = alarm_time + ALARM_DELTA;
+		power_on_alarm = alarm_time;
 	else
 		power_on_alarm = 0;
 }
@@ -120,9 +118,7 @@ void set_power_on_alarm(long secs, bool enable)
 	 *to power up the device before actual alarm
 	 *expiration
 	 */
-	if ((alarm_time - ALARM_DELTA) > rtc_secs)
-		alarm_time -= ALARM_DELTA;
-	else
+	if (alarm_time <= rtc_secs)
 		goto disable_alarm;
 
 	rtc_time_to_tm(alarm_time, &alarm.time);
