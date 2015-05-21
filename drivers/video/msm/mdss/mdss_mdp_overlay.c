dd90b7e154aef96458ab4b990e0f587f5785a5e5
Krishna Chaitanya Parimi <cparimi@codeaurora.org>
2015-05-21 18:48:32 +0530

    msm: mdss: remove notify update ioctl implementation in driver
    The notify update ioctl implementation is being switched to a
    sysfs based interfaces. The show_blank_event sysfs can be used
    to check the update and suspend cases. The no-update case is
    no longer required by userspace hence removing it. The power
    off completion can also make use of the suspend case of sysfs.
    
    A new backlight notification for ad_bl_level and bl_level is
    being added to support polling for their updates from userspace.
    
    Change-Id: Ib7a27d8057037067b4c2eb416c696e4a5ebe5f1d
    Signed-off-by: Krishna Chaitanya Parimi <cparimi@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_overlay.c
+++ drivers/video/msm/mdss/mdss_mdp_overlay.c
@@ -1319,8 +1319,6 @@ int mdss_mdp_overlay_kickoff(struct msm_fb_data_type *mfd,
 			}
 		}
 	}
-
-	mdss_fb_update_notify_update(mfd);
 commit_fail:
 	ATRACE_BEGIN("overlay_cleanup");
 	mdss_mdp_overlay_cleanup(mfd, &destroy_pipes);
@@ -2039,6 +2037,18 @@ static ssize_t mdss_mdp_vsync_show_event(struct device *dev,
 	return ret;
 }
 
+static ssize_t mdss_mdp_pp_bl_show_event(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct fb_info *fbi =  dev_get_drvdata(dev);
+	struct msm_fb_data_type *mfd = (struct msm_fb_data_type *)fbi->par;
+	int ret;
+
+	ret = scnprintf(buf, PAGE_SIZE, "bl_level=%u, ad_bl_level=%u\n",
+		mfd->bl_level, mfd->ad_bl_level);
+	return ret;
+}
+
 static inline int mdss_mdp_ad_is_supported(struct msm_fb_data_type *mfd)
 {
 	struct mdss_mdp_ctl *ctl = mfd_to_ctl(mfd);
@@ -2105,10 +2115,12 @@ static ssize_t mdss_mdp_ad_store(struct device *dev,
 static DEVICE_ATTR(vsync_event, S_IRUGO, mdss_mdp_vsync_show_event, NULL);
 static DEVICE_ATTR(ad, S_IRUGO | S_IWUSR | S_IWGRP, mdss_mdp_ad_show,
 	mdss_mdp_ad_store);
+static DEVICE_ATTR(pp_bl_event, S_IRUGO, mdss_mdp_pp_bl_show_event, NULL);
 
 static struct attribute *mdp_overlay_sysfs_attrs[] = {
 	&dev_attr_vsync_event.attr,
 	&dev_attr_ad.attr,
+	&dev_attr_pp_bl_event.attr,
 	NULL,
 };
 
